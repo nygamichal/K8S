89e3cf0a31017dc6349c20afa53a297ad0b85a7a
Michal Nyga <mnyga94@gmail.com>
2022-01-04 17:55:25 +0100

    Update run-vote-app.sh

--- vote-app/run-vote-app.sh
+++ vote-app/run-vote-app.sh
@@ -50,3 +50,5 @@ echo ""
 
 # # Please remember to replace <POD_NAME> with full name of result POD.
 # kubectl port-forward --address 0.0.0.0 pod/result-<POD_NAME> -n result 8080:80
+POD_RESULT_NAME=$(kubectl get pods -A --no-headers=true -o custom-columns=result:.metadata.name | grep result)
+kubectl port-forward --address 0.0.0.0 pod/$POD_RESULT_NAME -n result 8080:80
