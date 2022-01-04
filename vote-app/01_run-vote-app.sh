95f750f79b0b5502f40b4d085b6f1bf806886005
Michal Nyga <mnyga94@gmail.com>
2022-01-04 18:09:43 +0100

    Update and rename run-vote-app.sh to 01_run-vote-app.sh

--- vote-app/01_run-vote-app.sh
+++ vote-app/01_run-vote-app.sh
@@ -43,12 +43,3 @@ kubectl apply -f vote-deploy-result-panda.yaml
 echo "===List all services==="
 kubectl get svc -A
 echo ""
-# # @ @ @ Port-forward @ @ @ #
-# # socat is potentially required for port-forward
-# yum update && yum install -y socat
-
-
-# # Please remember to replace <POD_NAME> with full name of result POD.
-# kubectl port-forward --address 0.0.0.0 pod/result-<POD_NAME> -n result 8080:80
-POD_RESULT_NAME=$(kubectl get pods -A --no-headers=true -o custom-columns=result:.metadata.name | grep result)
-kubectl port-forward --address 0.0.0.0 pod/$POD_RESULT_NAME -n result 8080:80
